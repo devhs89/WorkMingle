38ee2dbedffa39f2db4e413c64f9fc8db4bae0d0
Bhavna <45494222+Bhavna0505@users.noreply.github.com>
2023-10-01 15:27:02 -0400

    WORK-1 setup express.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, function(){
+    console.log('App started on port 3000');
+});
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+
