555058b8450e705f75f15eddcfe233c9e222567b
Bhavna <45494222+Bhavna0505@users.noreply.github.com>
2023-10-01 15:36:39 -0400

    WORK-2 configure dotenv for environment variables

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 app.use(express.static('public'));
-app.listen(3000, function(){
-    console.log('App started on port 3000');
+
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGODB_URI;
+
+app.listen(port, function(){
+    console.log('App started on port '+port);
 });
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });
