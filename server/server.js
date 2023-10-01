3f301c6a304c2838cda5c1fd4369efd8f799cc99
Bhavna <45494222+Bhavna0505@users.noreply.github.com>
2023-10-01 15:50:21 -0400

    WORK-3 install and configure MongoDB

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ const mongoURI = process.env.MONGODB_URI;
 app.listen(port, function(){
     console.log('App started on port '+port);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });
 });
