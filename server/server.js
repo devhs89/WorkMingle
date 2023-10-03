323d99151afbe849e92e6e9e15fe51cddf9ed543
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-10-02 22:44:11 -0400

    WORK-1 manual Express server test complete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
-app.listen(3000, function(){
-    console.log('App started on port 3000');
-});
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+
+app.listen(3000, function () {
+  console.log('App started on port 3000');
 });
 
+app.get('/', (req, res) => {
+  res.json({message: 'Hello, World!'});
+});
