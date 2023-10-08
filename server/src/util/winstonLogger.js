31e1606c0fd8a1505b4f4c0c4479c85bf77165b5
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-10-08 19:10:00 -0400

    WORK-6 implement winston logger catching errors and logging

---
+++ server/src/util/winstonLogger.js
@@ -0,0 +1,47 @@
+﻿const winston = require('winston');
+const winstonDailyRotateFile = require('winston-daily-rotate-file');
+const path = require("path");
+const {combine, timestamp, json} = winston.format;
+
+const errorDailyRotateFile = new winstonDailyRotateFile({
+  frequency: '1d',
+  filename: path.join(require.main.path, 'logs', 'error-workmingle-%DATE%.log'),
+  level: 'error',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '30d'
+});
+
+const warnDailyRotateFile = new winstonDailyRotateFile({
+  frequency: '1d',
+  filename: path.join(require.main.path, 'logs', 'warn-workmingle-%DATE%.log'),
+  level: 'warn',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '30d'
+});
+
+const infoDailyRotateFile = new winstonDailyRotateFile({
+  frequency: '1d',
+  filename: path.join(require.main.path, 'logs', 'info-workmingle-%DATE%.log'),
+  level: 'info',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '30d'
+});
+
+const combinedDailyRotateFile = new winstonDailyRotateFile({
+  frequency: '1d',
+  filename: path.join(require.main.path, 'logs', 'combined-workmingle-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m',
+  maxFiles: '30d'
+});
+
+const logWithWinston = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()),
+  transports: [errorDailyRotateFile, warnDailyRotateFile, infoDailyRotateFile, combinedDailyRotateFile]
+});
+
+module.exports = logWithWinston;
