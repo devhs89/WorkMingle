4ea44f442cb18da7ee8ccda574ab25730e377d45
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-11-02 19:48:35 -0400

    WORK-22 create employer model

---
+++ server/src/models/employer.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employerSchema = new Schema({
+  userId: {type: Schema.Types.ObjectId, ref: 'AppUser'},
+  businessName: {type: String, required: true, unique: true},
+  industry: {type: String, required: true},
+  streetAddress: {type: String, required: true},
+  city: {type: String, required: true},
+  postalCode: {type: String, required: true},
+  country: {type: String, required: true},
+  website: {type: String, pattern: 'https?://.+'},
+  workEmail: {type: String, required: true, unique: true},
+  workPhone: {type: String},
+  description: {type: String},
+});
+const Employer = mongoose.model('Employer', employerSchema);
+module.exports = Employer;
