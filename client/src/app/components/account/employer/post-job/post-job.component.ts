ea42e1633996c130322913d5bf803d8b94d97ef7
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-11-06 18:30:26 -0500

    WORK-22 implement basic post job component and employer-features service

--- client/src/app/components/account/employer/post-job/post-job.component.ts
+++ client/src/app/components/account/employer/post-job/post-job.component.ts
@@ -1,10 +1,56 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {PageTitleService} from "../../../../services/page-title.service";
+import {EmployerFeaturesService} from "../../../../services/employer-features.service";
 
 @Component({
   selector: 'app-post-job',
   templateUrl: './post-job.component.html',
   styleUrls: ['./post-job.component.scss']
 })
-export class PostJobComponent {
+export class PostJobComponent implements OnInit {
+  jobForm: FormGroup;
+  titleCtrl: FormControl = new FormControl('', Validators.required);
+  companyCtrl: FormControl = new FormControl('', Validators.required);
+  locationCtrl: FormControl = new FormControl('', Validators.required);
+  descriptionCtrl: FormControl = new FormControl('', Validators.required);
+  salaryCtrl: FormControl = new FormControl('');
 
+  dummyData = {
+    title: 'Software Engineer',
+    company: 'Google',
+    location: 'Mountain View, CA',
+    description: 'We are looking for a software engineer to join our team.',
+    salary: '100,000'
+  };
+
+  constructor(private formBuilder: FormBuilder, pageService: PageTitleService, private employerFeaturesService: EmployerFeaturesService) {
+    pageService.setWindowTitle('Post Job');
+    pageService.setPageTitle('Advertise a Job');
+    this.jobForm = this.formBuilder.group({
+      title: this.titleCtrl,
+      company: this.companyCtrl,
+      location: this.locationCtrl,
+      description: this.descriptionCtrl,
+      salary: this.salaryCtrl
+    });
+  }
+
+  onPjSubmit() {
+    if (this.jobForm.valid) {
+      const jobData = this.jobForm.value;
+      this.employerFeaturesService.postJob(jobData).subscribe({
+        next: (resp) => {
+          console.log(resp);
+        },
+        error: () => {
+          console.log('Error posting job');
+        }
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.jobForm.patchValue(this.dummyData);
+  }
 }
---
+++ client/src/app/services/employer-features.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {JobAdvertInterface} from "../interfaces/job-advert.interface";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployerFeaturesService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  postJob(jobData: JobAdvertInterface) {
+    return this.httpClient.post('/api/employer/post-job', jobData);
+  }
+}
