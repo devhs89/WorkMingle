0babfbaeddd7678b79ec18a5beb36154744a08d6
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-11-05 22:57:23 -0500

    WORK-23 fix authResponse observable issue, where rxjs operator could not find value because not even nullable was being emitted

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,21 +6,26 @@ import {LoginComponent} from "./components/account/login/login.component";
 import {PageNotFoundComponent} from "./components/generic/page-not-found/page-not-found.component";
 import {ProfileComponent} from "./components/account/profile/profile.component";
 import {authenticationGuard} from "./gaurds/authentication.guard";
+import {authorizationGuard} from "./interceptors/authorization.guard";
+import {OnboardComponent} from "./components/account/employer/onboard/onboard.component";
+import {JobsListComponent} from "./components/jobs/jobs-list/jobs-list.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'jobs', component: JobsListComponent},
   {path: 'profile', canActivate: [authenticationGuard], component: ProfileComponent},
   {
     path: 'employer',
-    canMatch: [authenticationGuard],
-    loadChildren: () => import('../app/components/account/employer/employer.module').then((m) => m.EmployerModule)
-  },
-  {
-    path: 'jobs',
-    canMatch: [authenticationGuard],
-    loadChildren: () => import('../app/components/jobs/jobs.module').then((m) => m.JobsModule)
+    children: [
+      {path: 'onboard', canActivate: [authenticationGuard], component: OnboardComponent},
+      {
+        path: 'features',
+        canMatch: [authorizationGuard],
+        loadChildren: () => import('./components/account/employer/employer.module').then((m) => m.EmployerModule)
+      }
+    ]
   },
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
--- client/src/app/components/account/employer/employer.module.ts
+++ client/src/app/components/account/employer/employer.module.ts
@@ -5,18 +5,19 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {MatUiModule} from "../../../modules/mat-ui/mat-ui.module";
 import {ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
-import {authorizationGuard} from "../../../interceptors/authorization.guard";
+import {PostJobComponent} from "./post-job/post-job.component";
 
 const routes: Routes = [
-  {path: 'onboard', component: OnboardComponent},
-  {path: 'dashboard', canActivate: [authorizationGuard], component: DashboardComponent},
+  {path: 'post-job', component: PostJobComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [
     OnboardComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostJobComponent
   ],
   imports: [
     CommonModule,
